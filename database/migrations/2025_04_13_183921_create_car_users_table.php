65cda1637c3215af28492689798fb2392f431a2b
LuizAgt <luiz.augusto9922@outlook.com>
2025-04-13 16:28:09 -0300

    feat: implemented login and register

--- database/migrations/2025_04_13_183921_create_car_users_table.php
+++ database/migrations/2025_04_13_183921_create_car_users_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('car_users', function (Blueprint $table) {
+        Schema::create('car_user', function (Blueprint $table) {
             $table->id();
             $table->foreignId('car_id')->constrained('cars');
             $table->foreignId('user_id')->constrained('users');
@@ -26,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('car_users');
+        Schema::dropIfExists('car_user');
     }
 };
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,10 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::post('/login', [\App\Http\Controllers\AuthController::class, 'login'])
+    ->middleware(['throttle:10,1']) // Limit to 10 requests per minute
+    ->name('login');
+
+Route::post('/register', [\App\Http\Controllers\AuthController::class, 'register'])
+    ->middleware(['throttle:10,1']) // Limit to 10 requests per minute
+    ->name('register');
